138edb4094fa2ca50ac017cf1ac131ee9e3c9c7d
Pouya875 <pouya.pirouzmand1999@gmail.com>
2026-06-16 23:34:38 +0330

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,13 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import os
+
+TOKEN = os.environ.get("5331097825:AAEbhHtOG1zsj97eL1atQweZ3LGuc8nCKlw")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("سلام 👋 ربات روشن شد!")
+
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
